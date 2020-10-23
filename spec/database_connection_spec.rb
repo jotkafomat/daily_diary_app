6fdf1fae10bf56d7525f6f6444fd28b2349534c8
jotkafomat <k-j@xl.wp.pl>
2020-10-23 18:40:38 +0100

    write test sets up a connection to a database through PG

---
+++ spec/database_connection_spec.rb
@@ -0,0 +1,11 @@
+require 'database_connection_spec'
+
+describe DatabaseConnection do
+  describe '.setup' do
+    it 'sets up a connection to a database through PG' do
+      expect(PG).to receive(:connect).with(dbname: daily_diary_manager_test)
+
+      DatabaseConnection.setup('daily_diary_manager_test')
+    end
+  end
+end
